1e450aa34b513ccb29867adbcb55e7d9083e7073
graykode <nlkey2022@gmail.com>
2023-04-20 17:46:04 +0900

    feat: add polygon zkevm mainnet

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const NETWORK_RPC_URLS: { [chainId: string]: string } = {
   '1': 'https://rpc.ankr.com/eth',
   '5': 'https://rpc.ankr.com/eth_goerli',
   '137': 'https://rpc.ankr.com/polygon',
+  '1101': 'https://zkevm-rpc.com',
   '1422': 'https://rpc.deprecated.zkevm-test.net',
   '1442': 'https://rpc.public.zkevm-test.net',
   '42161': 'https://rpc.ankr.com/arbitrum',
